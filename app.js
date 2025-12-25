00b559a333b79e4bd96fff0ac01dd486e5e015a5
Yasith Samarakoon <samarakoonyasi21@gmail.com>
2025-12-25 23:29:58 +0530

    Change document innerHtml

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ function Clickbutton(){
     let btnText  = document.getElementById("txtGuessNumber").value;
 
     if(randomNumbers != btnText){
-      
+        
+        document.getElementById("correctGif").innerHTML = '<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDkybmFwNXNkazN4YWsxbDd1c3NzZXhiOHBkZnFhcmR1aDhlZHhoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NTEyfNzEmOSlEmiVVM/giphy.gif" class="img-fluid rounded mx-auto d-block " alt="...">'
+
         LblId.innerText = 'wrong';
     }else if(randomNumbers == btnText){
+
+        document.getElementById("correctGif").innerHTML = '<img src ="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3V2emJ5cnA1d25tMGdtem9kMmE5cHI4bXlneWQxenEyd21qem56ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/n5xOuk22LGZFZNPKpX/giphy.gif" class="img-fluid rounded mx-auto d-block " alt="...">'
         LblId.innerText = 'Win'
     }
     console.log(randomNumbers);
